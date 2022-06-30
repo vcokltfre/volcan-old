4120d4844110f910bf6d12bac4983711b7a69165
vcokltfre <vcokltfre@gmail.com>
2022-06-30 12:21:27 +0100

    feat: add user/role/guild levels

--- src/config/config.go
+++ src/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"os"
 
+	"github.com/bwmarrin/discordgo"
 	"gopkg.in/yaml.v3"
 )
 
@@ -25,4 +26,34 @@ type Guild struct {
 
 type BotConfig struct {
 	Guilds map[string]Guild `yaml:"guilds"`
+	Levels map[string]int   `yaml:"levels"`
+}
+
+// Get the permission level of a member.
+// This takes the highest available level they qualify for,
+// which can be set per user, level, and guild.
+func (c *BotConfig) GetLevel(member discordgo.Member) int {
+	level := 0
+
+	// Check if the user has a level set.
+	userLevel, ok := c.Levels[member.User.ID]
+	if ok {
+		level = userLevel
+	}
+
+	// Check each role to see if there's a higher level.
+	for _, role := range member.Roles {
+		roleLevel, ok := c.Levels[role]
+		if ok && roleLevel > level {
+			level = roleLevel
+		}
+	}
+
+	// Check if the guild has a level set.
+	guildLevel, ok := c.Levels[member.GuildID]
+	if ok && guildLevel > level {
+		level = guildLevel
+	}
+
+	return level
 }
