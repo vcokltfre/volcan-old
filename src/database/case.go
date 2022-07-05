dfec0adb57d6684a25a50d15abef2ccd23e0e4db
vcokltfre <vcokltfre@gmail.com>
2022-07-05 10:27:34 +0100

    chore: omit empty fields for JSON data

--- src/database/case.go
+++ src/database/case.go
@@ -11,16 +11,16 @@ const (
 )
 
 type Case struct {
-	ID        int    `json:"id" gorm:"primaryKey;autoIncrement;uniqueIndex"`
-	UserID    string `json:"user_id" gorm:"type:varchar(255);index"`
-	UserName  string `json:"user_name" gorm:"type:varchar(255)"`
-	ModID     string `json:"mod_id" gorm:"type:varchar(255);index"`
-	ModName   string `json:"mod_name" gorm:"type:varchar(255)"`
-	Type      string `json:"type" gorm:"type:varchar(255);index"`
-	Reason    string `json:"reason" gorm:"type:text"`
-	CreatedAt int64  `json:"created_at" gorm:"index"`
-	ExpiresAt int64  `json:"expires_at" gorm:"index"`
-	Notified  bool   `json:"notified" gorm:"type:boolean"`
-	MuteType  string `json:"mute_type" gorm:"type:varchar(255);index"`
-	Metadata  string `json:"metadata" gorm:"type:text"`
+	ID        int    `json:"id,omitempty" gorm:"primaryKey;autoIncrement;uniqueIndex"`
+	UserID    string `json:"user_id,omitempty" gorm:"type:varchar(255);index"`
+	UserName  string `json:"user_name,omitempty" gorm:"type:varchar(255)"`
+	ModID     string `json:"mod_id,omitempty" gorm:"type:varchar(255);index"`
+	ModName   string `json:"mod_name,omitempty" gorm:"type:varchar(255)"`
+	Type      string `json:"type,omitempty" gorm:"type:varchar(255);index"`
+	Reason    string `json:"reason,omitempty" gorm:"type:text"`
+	CreatedAt int64  `json:"created_at,omitempty" gorm:"index"`
+	ExpiresAt int64  `json:"expires_at,omitempty" gorm:"index"`
+	Notified  bool   `json:"notified,omitempty" gorm:"type:boolean"`
+	MuteType  string `json:"mute_type,omitempty" gorm:"type:varchar(255);index"`
+	Metadata  string `json:"metadata,omitempty" gorm:"type:text"`
 }
