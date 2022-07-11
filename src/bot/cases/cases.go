3a963efb10bb19722662aa310c15a7b932a288d9
vcokltfre <vcokltfre@gmail.com>
2022-07-11 13:33:38 +0100

    feat: make better case formatting

--- src/bot/cases/cases.go
+++ src/bot/cases/cases.go
@@ -2,11 +2,17 @@ package cases_cmd
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/vcokltfre/volcan/src/commands"
 	"github.com/vcokltfre/volcan/src/database"
+	"github.com/vcokltfre/volcan/src/utils"
 )
+
+var colours = map[string]int{
+	"warn": utils.ColourWarn,
+}
 
 func RegisterCommands() error {
 	err := commands.Handler.Register(getCase)
@@ -18,21 +24,21 @@ func RegisterCommands() error {
 }
 
 func createCaseEmbed(dbCase *database.Case) *discordgo.MessageEmbed {
+	caseDetail := []string{
+		fmt.Sprintf("**User:** %s (%s)", dbCase.UserName, dbCase.UserID),
+		fmt.Sprintf("**Moderator:** %s (%s)", dbCase.ModName, dbCase.ModID),
+		fmt.Sprintf("**Created:** <t:%d:F>", dbCase.CreatedAt),
+		fmt.Sprintf("**Reason:** %s", dbCase.Reason),
+	}
+
+	colour, ok := colours[dbCase.Type]
+	if !ok {
+		colour = utils.ColourInfo
+	}
+
 	return &discordgo.MessageEmbed{
-		Title: "Case #" + fmt.Sprint(dbCase.ID),
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:  "User",
-				Value: dbCase.UserName,
-			},
-			{
-				Name:  "Reason",
-				Value: dbCase.Reason,
-			},
-			{
-				Name:  "Moderator",
-				Value: dbCase.ModName,
-			},
-		},
+		Title:       fmt.Sprintf("Case #%d: %s", dbCase.ID, dbCase.Type),
+		Description: strings.Join(caseDetail, "\n"),
+		Color:       colour,
 	}
 }
---
+++ src/utils/colours.go
@@ -0,0 +1,6 @@
+package utils
+
+const (
+	ColourInfo = 0x87CEEB
+	ColourWarn = 0xFFFF00
+)
